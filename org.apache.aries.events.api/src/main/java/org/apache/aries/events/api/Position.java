edb9d30f65f7a5ee0fc8f3923163d48c78395a4e
tmaret <tmaret@adobe.com>
2019-01-03 15:33:22 +0100

    Define the positionToString signature explicitly in the API

--- org.apache.aries.events.api/src/main/java/org/apache/aries/events/api/Position.java
+++ org.apache.aries.events.api/src/main/java/org/apache/aries/events/api/Position.java
@@ -17,15 +17,13 @@
  */
 package org.apache.aries.events.api;
 
-import java.io.Serializable;
-
 /**
  * Position in a the topic.
  * E.g. For a kafka implementation this would be a list of (partition, offset) as we do not support partitions 
  * this could simply be like an offset.
  * TODO How do we provide ordering without being too specific?
  */
-public interface Position extends Serializable {
+public interface Position {
     long getOffset();
 
 
